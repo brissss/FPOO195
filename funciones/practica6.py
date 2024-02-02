6264574057e76ab688ab99f69c43bd0c1661ffee
brissss <148284979+brissss@users.noreply.github.com>
2024-02-01 23:36:20 -0600

    se finalizo la practica 6 de funciones.

--- funciones/practica6.py
+++ funciones/practica6.py
@@ -12,6 +12,52 @@ def main():
    
 main()
 
-#ejercicio 2
+#ejercicio 2 - terminado
+#Escribir una función que calcule el área de un círculo y otra que
+#calcule el volumen de un cilindro usando la primera función.
 
+"""def area_circulo(radio, pi = 3.1416):
+    return (pi * (radio*radio))
+
+def volumen_cilindro (area_final, altura):
+    return (area_final * altura)
+
+
+def main():
+    radio = float(input("ingrese el radio del circulo: "))
+    area_final = area_circulo (radio)
+    
+    print(f"el area del circulo es: {area_final}")
+    
+
+    altura = float(input("ingrese la altura del cilindro: "))
+    volumen_final = volumen_cilindro (area_final, altura)
+    
+    print(f"el volumen del cilindro es: {volumen_final}")
+
+main()"""
+
+#ejercicio 3
+#Escribir una función que convierta un número decimal en binario y
+#otra que convierta un número binario en decimal.
+
+"""def decimal_a_binario (decimal):
+  return bin(decimal).replace("0b", "")
+
+def binario_a_decimal (binario):
+    return int(binario, 2)
+
+def main():
+    decimal = int(input("ingrese un numero decimal entero: "))
+    conversion_decimal = decimal_a_binario (decimal)
+    
+    print (f"el numero decimal ingresado en binario es igual a: {conversion_decimal}")
+    
+
+    binario = input("ingrese un numero binario: ")
+    conversion_binario = binario_a_decimal (binario)
+    
+    print (f"el numero binario ingresado en decimal es: {conversion_binario}")
+    
+main()"""
 
