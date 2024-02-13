92e0b29097bcb25d2c6681f7486f65b358922941
brissss <148284979+brissss@users.noreply.github.com>
2024-02-12 20:29:38 -0600

    se termino el problema 1 de la practica 6.1

---
+++ listas_y_tuplas/practica6-1.py
@@ -0,0 +1,98 @@
+"""1. Sumatoria de los elementos de la lista
+2. Numero mayor de la lista
+3. Numero menor de la lista
+4. Promedio
+5. Moda: es el valor que más se repite de un conjunto de datos
+6. Rango: es la diferencia entre el valor máximo y el valor mínimo de un
+conjunto de datos"""
+
+def crear_tupla():
+  n = int(input("Ingrese los numeros que decea que contenga su tupla: "))
+  tupla = ()
+  for i in range(n):
+    numero = int(input(f"Ingrese el numero de la posicion {i + 1}: "))
+    tupla += (numero,)
+  return tupla
+
+
+def sumatoria(tupla):
+  sumatoria = 0
+  for numero in tupla:
+    sumatoria += numero
+  return sumatoria
+
+
+def numero_mayor(tupla):
+  numero_mayor = tupla[0]
+  for numero in tupla:
+    if numero > numero_mayor:
+      numero_mayor = numero
+  return numero_mayor
+
+
+def numero_menor(tupla):
+  numero_menor = tupla[0]
+  for numero in tupla:
+    if numero < numero_menor:
+      numero_menor = numero
+  return numero_menor
+
+
+def promedio(tupla):
+  sumatoria = sumatoria(tupla)
+  n = len(tupla)
+  promedio = sumatoria / n
+  return promedio
+
+
+def moda(tupla):
+  frecuencias = {}
+  for numero in tupla:
+    if numero not in frecuencias:
+      frecuencias[numero] = 1
+    else:
+      frecuencias[numero] += 1
+  moda = max(frecuencias, key=frecuencias.get)
+  return moda
+
+
+def rango(tupla):
+  numero_mayor = numero_mayor(tupla)
+  numero_menor = numero_menor(tupla)
+  rango = numero_mayor - numero_menor
+  return rango
+
+
+def menu():
+  print("menu funcional:")
+  print("1. Sumatoria de los elementos de la lista")
+  print("2. Numero mayor de la lista")
+  print("3. Numero menor de la lista")
+  print("4. Promedio")
+  print("5. Moda: es el valor que más se repite de un conjunto de datos")
+  print("6. Rango: es la diferencia entre el valor maximo y el valor mínimo de un conjunto de datos")
+  opcion = int(input("Elija una opción: "))
+  return opcion
+
+
+def main():
+  tupla = crear_tupla()
+  opcion = menu()
+  if opcion == 1:
+    print(f"La sumatoria de los elementos de la tupla es {sumatoria(tupla)}")
+  elif opcion == 2:
+    print(f"El número mayor de la tupla es {numero_mayor(tupla)}")
+  elif opcion == 3:
+    print(f"El número menor de la tupla es {numero_menor(tupla)}")
+  elif opcion == 4:
+    print(f"El promedio de los elementos de la tupla es {promedio(tupla)}")
+  elif opcion == 5:
+    print(f"La moda de la tupla es {moda(tupla)}")
+  elif opcion == 6:
+    print(f"El rango de la tupla es {rango(tupla)}")
+  else:
+    print("Opción no válida")
+
+
+if __name__ == "__main__":
+  main()
---
+++ listas_y_tuplas/practica6-2.py
@@ -0,0 +1,4 @@
+"""a. Contar número repetidos
+b. Eliminar numero repetidos
+c. Remplazar los repetidos con 0"""
+
